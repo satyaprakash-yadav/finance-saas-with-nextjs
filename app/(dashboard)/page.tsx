642196f9e02490b33fd796a136197d64748e2f24
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-08-07 14:25:40 +0530

    feat: Header component Added

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,6 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-      <UserButton afterSwitchSessionUrl="/" />
+      <div>
+        Dashboard Page! 
+      </div>
   );
 }
