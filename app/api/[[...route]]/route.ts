72bfa6ca480cdd2cc171cc543e7c0063ff03898b
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-08-08 13:47:39 +0530

    feat: Drizzle ORM & Neon Database / Accounts GET API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,28 +1,23 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
-import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import accounts from "./accounts"
+// import { HTTPException } from 'hono/http-exception';
 
 export const runtime = 'edge'
 
-const app = new Hono().basePath('/api')
+const app = new Hono().basePath('/api');
 
-app.get('/hello',
-  clerkMiddleware(),
-  (c) => {
-    const auth = getAuth(c)
+// app.onError((err, c) => {
+//     if (err instanceof HTTPException) {
+//         return err.getResponse();
+//     }
+//     return c.json({ error: "Internal Error" }, 500);
+// });
 
-    if (!auth?.userId) {
-      return c.json({
-        message: 'You are not logged in.'
-      })
-    }
-
-    return c.json({
-      message: 'You are logged in!',
-      userId: auth.userId
-    })
-  })
+const routes = app.route("/accounts", accounts)
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { QueryProvider } from "@/providers/query-provider";
+
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "./globals.css";
@@ -20,7 +22,11 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <QueryProvider>
+            {children}
+          </QueryProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
